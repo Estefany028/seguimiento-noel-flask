eb92bd32db22ee76783c93943085ba534556aab9
Estefany028 <estefanymb046@gmail.com>
2026-03-16 11:51:20 -0500

    Update run.py

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import os
 import threading
 import worker
 from app import app
@@ -11,4 +12,6 @@ if __name__ == "__main__":
 
     print("Worker iniciado en background")
 
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+
+    app.run(host="0.0.0.0", port=port)
